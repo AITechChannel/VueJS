c6f0912d2db2d4184a9f9231fb88be8ffcc5c3d1
AITechchannel <dta.bk13@gmail.com>
2022-08-04 12:02:50 +0700

    axios crud vue

--- src/components/Practice2.vue
+++ src/components/Practice2.vue
@@ -1,30 +1,68 @@
 <script setup>
 import Delete from "vue-material-design-icons/Delete.vue";
-import Menu from "vue-material-design-icons/Menu.vue";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import {
+  faUserSecret,
+  faPenToSquare,
+  faFloppyDisk,
+} from "@fortawesome/free-solid-svg-icons";
+import { mapActions } from "vuex";
+
+/* add icons to the library */
+library.add(faUserSecret, faPenToSquare, faFloppyDisk);
 
 components: {
   Delete;
-  Menu;
+
+  // FontAwesomeIcon;
 }
 </script>
 
 <template>
   <div>
-    <div><button @click="increment()">Click</button></div>
-    <h1>{{ this.$store.state.count }}</h1>
-
-    <div>
+    <div class="input-container">
       <input v-model="todoInput" ref="todoInput" />
-      <button @click="addTodo(todoInput)">ADD</button>
+      <button @click="createTask(todoInput)">ADD</button>
     </div>
 
-    <div v-for="todo in this.$store.state.todos">
+    <div class="list-container" v-for="todo in this.$store.state.todos">
       <ul>
-        <li @click="delTodo(todo.id)">{{ todo.content }}</li>
-        <span><Menu /></span>
-        <span><Delete /></span>
+        <li class="list-item">
+          <span
+            ref="contentRef"
+            class="content"
+            :contenteditable="todo.contenteditable"
+            >{{ todo.content }}</span
+          >
+
+          <div class="icon-container">
+            <span
+              class="icon"
+              v-if="todo.contenteditable"
+              @click="setContenteditable(todo.id)"
+            >
+              <font-awesome-icon icon="fa-solid fa-floppy-disk" />
+            </span>
+
+            <span class="icon" v-else @click="setContenteditable(todo.id)">
+              <font-awesome-icon icon="fa-solid fa-pen-to-square" />
+            </span>
+
+            <span class="icon" @click="delTodo(todo.id)">
+              <Delete />
+            </span>
+          </div>
+        </li>
       </ul>
     </div>
+
+    <div><button @click="increment()">Increase</button></div>
   </div>
 </template>
 
@@ -32,32 +70,84 @@ components: {
 export default {
   data() {
     return {
-      storeData: 0,
       todoInput: "",
     };
   },
 
   methods: {
-    increment() {
-      this.$store.commit("increment");
-      this.storeData = this.$store.state.count;
-    },
-    addTodo(valueTodo) {
-      this.$store.commit("addTodo", valueTodo);
-      console.log(this.$store.state);
-      this.todoInput = "";
-      this.$refs.todoInput.focus();
+    createTask(valueTodo) {
+      if (this.todoInput.trim() !== "") {
+        this.$store.commit("addTodo", valueTodo);
+        console.log(this.$store.state);
+        this.todoInput = "";
+        this.$refs.todoInput.focus();
+        this.$store.dispatch("createTask", { content: valueTodo });
+      }
     },
     delTodo(id) {
       this.$store.commit("delTodo", id);
     },
+    setContenteditable(id) {
+      this.$store.commit("toggleContenteditable", id);
+      this.$refs.contentRef.focus();
+    },
+    // ...mapActions(["createTask"]),
   },
 };
 </script>
 
-<style>
-button {
-  width: 50px;
-  height: 50px;
+<style lang="scss">
+.input-container {
+  display: flex;
+  margin-bottom: 20px;
+  input {
+    flex: 1;
+    margin-right: 10px;
+    padding: 0 10px;
+    border-radius: 10px;
+    border: 1px solid #777;
+    outline: none;
+  }
+  input:focus {
+    border: 1px solid rgb(55, 189, 212);
+  }
+
+  button {
+    padding: 0.6rem 1rem;
+    border-radius: 10px;
+    border: 1px solid #777;
+  }
+}
+
+.list-container {
+  ul {
+    padding-left: 0;
+  }
+  .list-item {
+    display: flex;
+    align-items: center;
+
+    .content {
+      padding: 4px 10px;
+    }
+  }
+}
+
+.icon-container {
+  display: flex;
+  align-items: center;
+  span {
+    display: flex;
+    align-items: center;
+  }
+
+  .icon:first-of-type {
+    margin-right: 10px;
+  }
+
+  .icon:hover {
+    color: #41b883;
+    cursor: pointer;
+  }
 }
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,22 @@
 import { createApp } from "vue";
 import { createStore } from "vuex";
 
+import taskApi from "../api/taskApi";
+
 const store = createStore({
   state: {
-    count: 0,
     todos: [
       {
         id: 0,
-        content: "coding",
+        content: "Task 1",
         complete: false,
+        contenteditable: false,
       },
       {
         id: 2,
-        content: "coding",
+        content: "Task 2",
         complete: false,
+        contenteditable: false,
       },
     ],
   },
@@ -22,21 +25,60 @@ const store = createStore({
       state.count++;
     },
     addTodo: function (state, valueTodo) {
+      if (state.todos.length > 0) {
+        var id = state.todos.slice(-1)[0].id + 1;
+      }
       state.todos.push({
-        id: state.todos.slice(-1)[0].id + 1,
+        id: id || 1,
         content: valueTodo,
       });
     },
-
     delTodo: function (state, idTodo) {
       const indexTodo = state.todos.indexOf(store.getters.thisTodo(idTodo));
 
       state.todos.splice(indexTodo, 1);
     },
+    toggleContenteditable: function (state, idTodo) {
+      const indexTodo = state.todos.indexOf(store.getters.thisTodo(idTodo));
+
+      state.todos[indexTodo].contenteditable =
+        !state.todos[indexTodo].contenteditable;
+    },
   },
   getters: {
     thisTodo: (state) => (idTodo) => {
       return state.todos.find((todo) => todo.id === idTodo);
+    },
+  },
+  actions: {
+    // async increment({ commit }) {
+    //   // commit("increment");
+    //   // console.log("action");
+
+    //   // fetch("https://api.coindesk.com/v1/bpi/currentprice.json").then((res) =>
+    //   //   res.json().then((data) => console.log(data))
+    //   // );
+
+    //   // commit("addTodo");
+
+    //   try {
+    //     // const data = await axiosApi.get(`posts`);
+    //     // console.log(data);
+
+    //     const data = await taskApi.createTask({ content: "abc" });
+    //     console.log(data);
+    //     commit("addTodo");
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // },
+
+    async createTask({ commit, state }, payload) {
+      try {
+        const data = await taskApi.createTask(payload);
+        console.log(data);
+        console.log(payload);
+      } catch (error) {}
     },
   },
 });
